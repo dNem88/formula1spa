779b3f2c7e7c9a8f19013cbde0d91a1158005217
dNem88 <82020442+dNem88@users.noreply.github.com>
2022-02-19 10:12:22 +0200

    scroll top news

--- src/components/news/layout/NewsLayout.js
+++ src/components/news/layout/NewsLayout.js
@@ -7,15 +7,19 @@ import News from '../../common/news/News'
 
 function NewsLayout() {
     const news = useContext(newsContext)
-    console.log(news, 'NEWSLAYOUT')
+    
+    function scrollToTopSmoothly() {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        })
+    }
     
     return (
         <section className={styles.wrapper}>
             <Outlet/>
             <div className={styles['news-container']}>
-                <section onClick={() => {
-                    window.scrollTo(0,0)
-                }} className={styles['news-slider']}>
+                <section onClick={scrollToTopSmoothly} className={styles['news-slider']}>
                     {news.articles ? news.articles.map(x => {
                         return <News key={x._id} id={x._id} imageUrl={x.urlToImage} title={x.title} hoverChange={true}/>
                     }) : null}
